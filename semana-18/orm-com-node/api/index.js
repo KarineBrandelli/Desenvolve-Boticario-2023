b77f23a97d762ca67c817cf29f7b7e1918a03d62
Karine Brandelli <karinebrandelli@live.com>
2023-05-24 13:00:07 -0300

    semana-18(orm-com-node): escopo de associação, transações e refatoração do projeto

--- semana-18/orm-com-node/api/index.js
+++ semana-18/orm-com-node/api/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
 const port = 5010;
 
+app.use(cors());
 routes(app);
 
 app.listen(port, () => console.log(`servidor está rodando na porta ${port}`));
